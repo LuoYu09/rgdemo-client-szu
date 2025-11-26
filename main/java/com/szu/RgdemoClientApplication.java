7536d278ace5aede5f51e3534645419ab4bb2276
LuoYu <2509124889@qq.com>
2025-11-26 10:47:44 +0800

    Initial code

---
+++ main/java/com/szu/RgdemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.szu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgdemoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgdemoClientApplication.class, args);
+    }
+
+}
